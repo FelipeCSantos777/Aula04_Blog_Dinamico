bd3000e553171623549de7c94fac577aadf8f3bf
Felipe C Santos <felipecsantos777@gmail.com>
2024-08-14 10:25:55 -0300

    modelo_miniblog_140824.01

--- scripts.js
+++ scripts.js
@@ -24,11 +24,13 @@ for (var i=0; i<posts.length; i++) {
 
     const title = document.createElement('h2');
     title.classList.add('article-title');
+
     const titleText = document.createTextNode(postData.title);
     title.appendChild(titleText);
     article.appendChild(title);
     
     const content = document.createElement('p');
+    
     const contentText = document.createTextNode(postData.content);
     content.appendChild(contentText);
     article.appendChild(content);
